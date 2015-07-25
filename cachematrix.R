44833c5300e58f82e43e9f07fc35a5b76f58fbbf
JohnBSV <juan.bustamanteing@gmail.com>
2015-07-24 23:55:39 -0600

    Added a few lines to the functions
    At this point functions should be "functional". Comments are yet to be
    added

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,27 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  setmat<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  getmat<-function() x
+  setinv<-function(inv) m<<-inv
+  getinv<-function() m
+  list(set=setmat,get=getmat,getinv=getinv,setinv=setinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getinv()
+  if(!is.null(m)){
+    message("Getting cached result")
+    return(m)
+  }     ## Return a matrix that is the inverse of 'x'
+  matriz<-x$get()
+  m<-solve(matriz, ...)
+  x$setinv(m)
 }
